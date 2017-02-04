ba76e1e99387bf8de3027f19abcbc71719f27f5b
Anthony Erlinger <aerlinger@gmail.com>
2017-02-04 11:41:09 -0500

    Add dismissable alert

--- src/components/EditPane.js
+++ src/components/EditPane.js
@@ -16,123 +16,123 @@ class EditPane extends React.Component {
 
   render() {
     return (
-        <form style={{background: "#EEE"}}>
-          <img src={TransformerImage} width="60px" height="60px"/>
+        <form style={{background: '#EEE'}}>
+          <img src={TransformerImage} width='60px' height='60px'/>
           <h4>Transformer</h4>
           <hr />
 
-          <Bootstrap.FormGroup controlId="formValidationSuccess1" validationState="success">
+          <Bootstrap.FormGroup controlId='formValidationSuccess1' validationState='success'>
             <Bootstrap.ControlLabel>Input with success</Bootstrap.ControlLabel>
-            <Bootstrap.FormControl type="text" />
+            <Bootstrap.FormControl type='text' />
             <Bootstrap.HelpBlock>Help text with validation state.</Bootstrap.HelpBlock>
           </Bootstrap.FormGroup>
 
-          <Bootstrap.FormGroup controlId="formValidationWarning1" validationState="warning">
+          <Bootstrap.FormGroup controlId='formValidationWarning1' validationState='warning'>
             <Bootstrap.ControlLabel>Input with warning</Bootstrap.ControlLabel>
-            <Bootstrap.FormControl type="text" />
+            <Bootstrap.FormControl type='text' />
           </Bootstrap.FormGroup>
 
-          <Bootstrap.FormGroup controlId="formValidationError1" validationState="error">
+          <Bootstrap.FormGroup controlId='formValidationError1' validationState='error'>
             <Bootstrap.ControlLabel>Input with error</Bootstrap.ControlLabel>
-            <Bootstrap.FormControl type="text" />
+            <Bootstrap.FormControl type='text' />
           </Bootstrap.FormGroup>
 
-          <Bootstrap.FormGroup controlId="formValidationNull" validationState={null}>
+          <Bootstrap.FormGroup controlId='formValidationNull' validationState={null}>
             <Bootstrap.ControlLabel>Input with no validation state</Bootstrap.ControlLabel>
-            <Bootstrap.FormControl type="text" />
+            <Bootstrap.FormControl type='text' />
           </Bootstrap.FormGroup>
 
-          <Bootstrap.FormGroup controlId="formValidationSuccess2" validationState="success">
+          <Bootstrap.FormGroup controlId='formValidationSuccess2' validationState='success'>
             <Bootstrap.ControlLabel>Input with success and feedback icon</Bootstrap.ControlLabel>
-            <Bootstrap.FormControl type="text" />
+            <Bootstrap.FormControl type='text' />
             <Bootstrap.FormControl.Feedback />
           </Bootstrap.FormGroup>
 
-          <Bootstrap.FormGroup controlId="formValidationWarning2" validationState="warning">
+          <Bootstrap.FormGroup controlId='formValidationWarning2' validationState='warning'>
             <Bootstrap.ControlLabel>Input with warning and feedback icon</Bootstrap.ControlLabel>
-            <Bootstrap.FormControl type="text" />
+            <Bootstrap.FormControl type='text' />
             <Bootstrap.FormControl.Feedback />
           </Bootstrap.FormGroup>
 
-          <Bootstrap.FormGroup controlId="formValidationError2" validationState="error">
+          <Bootstrap.FormGroup controlId='formValidationError2' validationState='error'>
             <Bootstrap.ControlLabel>Input with error and feedback icon</Bootstrap.ControlLabel>
-            <Bootstrap.FormControl type="text" />
+            <Bootstrap.FormControl type='text' />
             <Bootstrap.FormControl.Feedback />
           </Bootstrap.FormGroup>
 
-          <Bootstrap.FormGroup controlId="formValidationSuccess3" validationState="success">
+          <Bootstrap.FormGroup controlId='formValidationSuccess3' validationState='success'>
             <Bootstrap.ControlLabel>Input with success and custom feedback icon</Bootstrap.ControlLabel>
-            <Bootstrap.FormControl type="text" />
+            <Bootstrap.FormControl type='text' />
             <Bootstrap.FormControl.Feedback>
-              <Bootstrap.Glyphicon glyph="music" />
+              <Bootstrap.Glyphicon glyph='music' />
             </Bootstrap.FormControl.Feedback>
           </Bootstrap.FormGroup>
 
-          <Bootstrap.FormGroup controlId="formValidationWarning3" validationState="warning">
+          <Bootstrap.FormGroup controlId='formValidationWarning3' validationState='warning'>
             <Bootstrap.ControlLabel>Input group with warning</Bootstrap.ControlLabel>
             <Bootstrap.InputGroup>
               <Bootstrap.InputGroup.Addon>@</Bootstrap.InputGroup.Addon>
-              <Bootstrap.FormControl type="text" />
+              <Bootstrap.FormControl type='text' />
             </Bootstrap.InputGroup>
             <Bootstrap.FormControl.Feedback />
           </Bootstrap.FormGroup>
 
-          <Bootstrap.Form componentClass="fieldset" horizontal>
-            <Bootstrap.FormGroup controlId="formValidationError3" validationState="error">
+          <Bootstrap.Form componentClass='fieldset' horizontal>
+            <Bootstrap.FormGroup controlId='formValidationError3' validationState='error'>
               <Bootstrap.Col componentClass={Bootstrap.ControlLabel} xs={3}>
                 Input with error
               </Bootstrap.Col>
               <Bootstrap.Col xs={9}>
-                <Bootstrap.FormControl type="text" />
+                <Bootstrap.FormControl type='text' />
                 <Bootstrap.FormControl.Feedback />
               </Bootstrap.Col>
             </Bootstrap.FormGroup>
 
-            <Bootstrap.FormGroup controlId="formValidationSuccess4" validationState="success">
+            <Bootstrap.FormGroup controlId='formValidationSuccess4' validationState='success'>
               <Bootstrap.Col componentClass={Bootstrap.ControlLabel} xs={3}>
                 Input group with success
               </Bootstrap.Col>
               <Bootstrap.Col xs={9}>
                 <Bootstrap.InputGroup>
                   <Bootstrap.InputGroup.Addon>@</Bootstrap.InputGroup.Addon>
-                  <Bootstrap.FormControl type="text" />
+                  <Bootstrap.FormControl type='text' />
                 </Bootstrap.InputGroup>
                 <Bootstrap.FormControl.Feedback />
               </Bootstrap.Col>
             </Bootstrap.FormGroup>
           </Bootstrap.Form>
 
-          <Bootstrap.Form componentClass="fieldset" inline>
-            <Bootstrap.FormGroup controlId="formValidationWarning4" validationState="warning">
+          <Bootstrap.Form componentClass='fieldset' inline>
+            <Bootstrap.FormGroup controlId='formValidationWarning4' validationState='warning'>
               <Bootstrap.ControlLabel>Input with warning</Bootstrap.ControlLabel>
               {' '}
-              <Bootstrap.FormControl type="text" />
+              <Bootstrap.FormControl type='text' />
               <Bootstrap.FormControl.Feedback />
             </Bootstrap.FormGroup>
             {' '}
-            <Bootstrap.FormGroup controlId="formValidationError4" validationState="error">
+            <Bootstrap.FormGroup controlId='formValidationError4' validationState='error'>
               <Bootstrap.ControlLabel>Input group with error</Bootstrap.ControlLabel>
               {' '}
               <Bootstrap.InputGroup>
                 <Bootstrap.InputGroup.Addon>@</Bootstrap.InputGroup.Addon>
-                <Bootstrap.FormControl type="text" />
+                <Bootstrap.FormControl type='text' />
               </Bootstrap.InputGroup>
               <Bootstrap.FormControl.Feedback />
             </Bootstrap.FormGroup>
           </Bootstrap.Form>
 
-          <Bootstrap.Checkbox validationState="success">
+          <Bootstrap.Checkbox validationState='success'>
             Checkbox with success
           </Bootstrap.Checkbox>
-          <Bootstrap.Radio validationState="warning">
+          <Bootstrap.Radio validationState='warning'>
             Radio with warning
           </Bootstrap.Radio>
-          <Bootstrap.Checkbox validationState="error">
+          <Bootstrap.Checkbox validationState='error'>
             Checkbox with error
           </Bootstrap.Checkbox>
 
           {/* This requires React 15's <span>-less spaces to be exactly correct. */}
-          <Bootstrap.FormGroup validationState="success">
+          <Bootstrap.FormGroup validationState='success'>
             <Bootstrap.Checkbox inline>
               Checkbox
             </Bootstrap.Checkbox>
@@ -146,7 +146,7 @@ class EditPane extends React.Component {
             </Bootstrap.Checkbox>
           </Bootstrap.FormGroup>
 
-          <Bootstrap.Button type="submit">
+          <Bootstrap.Button type='submit'>
             Sign in
           </Bootstrap.Button>
         </form>
--- src/components/Main.js
+++ src/components/Main.js
@@ -5,6 +5,7 @@ import TopNav from './TopNav';
 import SidePanel from './SidePanel';
 import EditPane from './EditPane';
 import ComponentButtonGroup from './ComponentButtonGroup';
+import AlertDismissable from './Alert';
 
 import {Grid, Row, Col, Button} from 'react-bootstrap';
 
@@ -12,15 +13,12 @@ import React from 'react';
 
 let yeomanImage = require('../images/yeoman.png');
 
-const buttonsInstance = (
-    <Button>Click me!</Button>
-);
-
 class AppComponent extends React.Component {
   render() {
     return (
       <div className='index'>
         <TopNav/>
+
         <Grid fluid={true}>
           <Row>
             <Col sm={3} md={2} lg={2}>
@@ -29,10 +27,10 @@ class AppComponent extends React.Component {
             </Col>
 
             <Col sm={6} md={8} lg={8}>
+              <AlertDismissable />
+
               <img src={yeomanImage} alt='Yeoman Generator'/>
               <div className='notice'>Please edit <code>src/components/Main.js</code> to get started!</div>
-
-              {buttonsInstance}
             </Col>
 
             <Col sm={3} md={2} lg={2}>
