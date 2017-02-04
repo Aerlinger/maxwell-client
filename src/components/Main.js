840571894ba3c5d28bcf25f8cec2fd7e1cc19a38
Anthony Erlinger <aerlinger@gmail.com>
2017-02-04 09:04:59 -0500

    Add SidePanel component

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,8 +2,9 @@ require('normalize.css/normalize.css');
 require('styles/App.css');
 
 import TopNav from './TopNav';
+import SidePanel from './SidePanel';
 
-import { Button } from 'react-bootstrap';
+import {Grid, Row, Col, Button} from 'react-bootstrap';
 
 import React from 'react';
 
@@ -18,17 +19,25 @@ class AppComponent extends React.Component {
     return (
       <div className='index'>
         <TopNav/>
+        <Grid fluid={true}>
+          <Row>
+            <Col sm={3} md={2} lg={1}>
+              <SidePanel />
+            </Col>
 
-        <img src={yeomanImage} alt='Yeoman Generator' />
-        <div className='notice'>Please edit <code>src/components/Main.js</code> to get started!</div>
+            <Col sm={6} md={3} lg={9}>
+              <img src={yeomanImage} alt='Yeoman Generator'/>
+              <div className='notice'>Please edit <code>src/components/Main.js</code> to get started!</div>
 
-        {buttonsInstance}
+              {buttonsInstance}
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
 }
 
-AppComponent.defaultProps = {
-};
+AppComponent.defaultProps = {};
 
 export default AppComponent;
---
+++ src/components/SidePanel.js
@@ -0,0 +1,30 @@
+require('normalize.css/normalize.css');
+
+import { ListGroup, ListGroupItem } from 'react-bootstrap';
+
+import React from 'react';
+
+class SidePanel extends React.Component {
+  render() {
+    return (
+      <ListGroup>
+        <ListGroupItem href="#" active>Link 1</ListGroupItem>
+        <ListGroupItem href="#">Link 2</ListGroupItem>
+        <ListGroupItem href="#" disabled>Link 3</ListGroupItem>
+
+        <ListGroupItem bsStyle="success">Success</ListGroupItem>
+        <ListGroupItem bsStyle="info">Info</ListGroupItem>
+        <ListGroupItem bsStyle="warning">Warning</ListGroupItem>
+        <ListGroupItem bsStyle="danger">Danger</ListGroupItem>
+
+        <ListGroupItem header="Heading 1">Some body text</ListGroupItem>
+        <ListGroupItem header="Heading 2" href="#">Linked item</ListGroupItem>
+        <ListGroupItem header="Heading 3" bsStyle="danger">Danger styling</ListGroupItem>
+      </ListGroup>
+    );
+  }
+}
+
+SidePanel.defaultProps = {};
+
+export default SidePanel;
