bfd587d4d5dcfbc84e3c824432e0a060263a43eb
Anthony Erlinger <aerlinger@gmail.com>
2017-02-04 10:17:52 -0500

    Update ComponentButtonGroup

--- src/components/ComponentButtonGroup.js
+++ src/components/ComponentButtonGroup.js
@@ -1,21 +1,26 @@
-require('normalize.css/normalize.css');
+require('!!raw!normalize.css/normalize.css');
 
 import { Accordion, Panel } from 'react-bootstrap';
+import ComponentButton from './ComponentButton';
 
 import React from 'react';
 
 class ComponentButtonGroup extends React.Component {
   render() {
     return (
-      <Accordion activeKey={"1"} defaultActiveKey={"1"}>
-        <Panel header="Collapsible Group Item #1" eventKey="1" expanded={true} defaultExpanded={true}>
-          Button
+      <Accordion activeKey={'1'} defaultActiveKey={'1'}>
+        <Panel header='Collapsible Group Item #1' eventKey='1' expanded={true} defaultExpanded={true}>
+          <ComponentButton />
+          <ComponentButton />
+          <ComponentButton />
+          <ComponentButton />
+          <ComponentButton />
         </Panel>
-        <Panel header="Collapsible Group Item #2" eventKey="2">
-          Buttons2
+        <Panel header='Collapsible Group Item #2' eventKey='2'>
+          <ComponentButton />
         </Panel>
-        <Panel header="Collapsible Group Item #3" eventKey="3">
-          Buttons3
+        <Panel header='Collapsible Group Item #3' eventKey='3'>
+          <ComponentButton />
         </Panel>
       </Accordion>
     );
