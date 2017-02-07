9864d755fe858690799ab3e31bf383f65c9d67e3
Anthony Erlinger <aerlinger@gmail.com>
2017-02-07 11:10:05 -0500

    Add validation to sign up form

--- src/api/controllers/authentication.js
+++ src/api/controllers/authentication.js
@@ -1,11 +1,83 @@
 var passport = require('passport');
 var mongoose = require('mongoose');
 var User = mongoose.model('User');
+const validator = require('validator');
 
 var sendJSONresponse = function(res, status, content) {
   res.status(status);
   res.json(content);
 };
+
+/**
+ * Validate the sign up form
+ *
+ * @param {object} payload - the HTTP body message
+ * @returns {object} The result of validation. Object contains a boolean validation result,
+ *                   errors tips, and a global message for the whole form.
+ */
+function validateSignupForm(payload) {
+  const errors = {};
+  let isFormValid = true;
+  let message = '';
+
+  if (!payload || typeof payload.email !== 'string' || !validator.isEmail(payload.email)) {
+    isFormValid = false;
+    errors.email = 'Please provide a correct email address.';
+  }
+
+  if (!payload || typeof payload.password !== 'string' || payload.password.trim().length < 8) {
+    isFormValid = false;
+    errors.password = 'Password must have at least 8 characters.';
+  }
+
+  if (!payload || typeof payload.name !== 'string' || payload.name.trim().length === 0) {
+    isFormValid = false;
+    errors.name = 'Please provide your name.';
+  }
+
+  if (!isFormValid) {
+    message = 'Check the form for errors.';
+  }
+
+  return {
+    success: isFormValid,
+    message,
+    errors
+  };
+}
+
+/**
+ * Validate the login form
+ *
+ * @param {object} payload - the HTTP body message
+ * @returns {object} The result of validation. Object contains a boolean validation result,
+ *                   errors tips, and a global message for the whole form.
+ */
+function validateLoginForm(payload) {
+  const errors = {};
+  let isFormValid = true;
+  let message = '';
+
+  if (!payload || typeof payload.email !== 'string' || payload.email.trim().length === 0) {
+    isFormValid = false;
+    errors.email = 'Please provide your email address.';
+  }
+
+  if (!payload || typeof payload.password !== 'string' || payload.password.trim().length === 0) {
+    isFormValid = false;
+    errors.password = 'Please provide your password.';
+  }
+
+  if (!isFormValid) {
+    message = 'Check the form for errors.';
+  }
+
+  return {
+    success: isFormValid,
+    message,
+    errors
+  };
+}
 
 module.exports.register = function(req, res) {
 
@@ -16,29 +88,36 @@ module.exports.register = function(req, res) {
   //   return;
   // }
 
-  console.log("REGISTER req:", req.body);
+  const validationResult = validateSignupForm(req.body);
 
-  let user = new User();
+  if (!validationResult.success) {
+    return res.status(400).json({
+      success: false,
+      message: validationResult.message,
+      errors: validationResult.errors
+    });
+  } else {
+    let user = new User();
 
-  user.name = req.body.name;
-  user.email = req.body.email;
+    user.name = req.body.name;
+    user.email = req.body.email;
 
-  user.setPassword(req.body.password);
+    user.setPassword(req.body.password);
 
-  user.save(function(err) {
-    if (!err) {
+    user.save(function (err) {
+      if (!err) {
 
-      var token;
-      token = user.generateJwt();
-      res.status(200);
-      res.json({
-        "token": token
-      });
-    } else {
-      console.error(err);
-    }
-  });
-
+        var token;
+        token = user.generateJwt();
+        res.status(200);
+        res.json({
+          "token": token
+        });
+      } else {
+        console.error(err);
+      }
+    });
+  }
 };
 
 module.exports.login = function(req, res) {
@@ -50,12 +129,14 @@ module.exports.login = function(req, res) {
   //   return;
   // }
 
+  const validationResult = validateLoginForm(req.body);
+
   passport.authenticate('local', function(err, user, info){
     var token;
 
     // If Passport throws/catches an error
     if (err) {
-      res.status(404).json(err);
+      res.status(401).json(err);
       return;
     }
 
