01495a11602dd1a49b73b04a5e814ff5a8a5d7f0
Anthony Erlinger <aerlinger@gmail.com>
2017-02-07 22:30:23 -0500

    Initial format of Circuit schema

--- src/api/models/circuit.js
+++ src/api/models/circuit.js
@@ -1,10 +1,26 @@
 let mongoose = require( 'mongoose' );
 
+var SimParamsSchema = new mongoose.Schema({
+  name: String,
+  simSpeed: Number,
+  currentSpeed: Number,
+  voltageRange: Number,
+  powerRange: Number,
+  flags: Number,
+  created_at: Date,
+  updated_at: Date
+});
+
+var ComponentParamsSchema = new mongoose.Schema({
+  name: String,
+  pos: [Number],
+  flags: Number,
+  params: {}
+});
+
 let CircuitSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  }
+  params: SimParamsSchema,
+  components: [ComponentParamsSchema]
 });
 
 module.exports = mongoose.model('Circuit', CircuitSchema);
