04cc55dac0941a07900de795e5bc6ed5a4b5c014
Anthony Erlinger <aerlinger@gmail.com>
2017-02-11 18:34:24 -0500

    Update ComponentPane to populate from object data

--- src/components/ComponentPane.js
+++ src/components/ComponentPane.js
@@ -1,83 +1,100 @@
 import React from 'react';
 
-import Subheader from 'material-ui/Subheader';
 import Drawer from 'material-ui/Drawer';
-import {GridList, GridTile} from 'material-ui/GridList';
-import IconButton from 'material-ui/IconButton';
-import StarBorder from 'material-ui/svg-icons/toggle/star-border';
 
-import ComponentButton from '../components/ComponentButton'
+import {Card, CardHeader, CardText} from 'material-ui/Card';
+import ComponentButton from '../components/ComponentButton';
 
 const styles = {
   root: {
     display: 'flex',
     flexWrap: 'wrap',
-    justifyContent: 'space-around',
+    justifyContent: 'space-around'
   },
   gridList: {
     width: 500,
     height: 900,
-    overflowY: 'auto',
-  },
+    overflowY: 'auto'
+  }
 };
 
 const tilesData = [
   {
-    img: 'images/yeoman.png',
-    title: 'Breakfast',
-    author: 'jill111',
+    sectionName: "Basic",
+    expanded: true,
+    components: [
+      {
+        img: 'images/yeoman.png',
+        title: 'Transformer',
+        hotkey: 'T'
+      },
+      {
+        img: 'images/yeoman.png',
+        title: 'Resistor',
+        hotkey: 'R'
+      },
+      {
+        img: 'images/yeoman.png',
+        title: 'Wire',
+        hotkey: 'W'
+      }
+    ]
   },
   {
-    img: 'images/grid-list/burger-827309_640.jpg',
-    title: 'Tasty burger',
-    author: 'pashminu',
-  },
-  {
-    img: 'images/grid-list/camera-813814_640.jpg',
-    title: 'Camera',
-    author: 'Danson67',
-  },
-  {
-    img: 'images/grid-list/morning-819362_640.jpg',
-    title: 'Morning',
-    author: 'fancycrave1',
-  },
-  {
-    img: 'images/grid-list/hats-829509_640.jpg',
-    title: 'Hats',
-    author: 'Hans',
-  },
-  {
-    img: 'images/grid-list/honey-823614_640.jpg',
-    title: 'Honey',
-    author: 'fancycravel',
-  },
-  {
-    img: 'images/yeoman.jpg',
-    title: 'Vegetables',
-    author: 'jill111',
-  },
-  {
-    img: 'images/grid-list/water-plant-821293_640.jpg',
-    title: 'Water plant',
-    author: 'BkrmadtyaKarki',
-  },
+    sectionName: "Digital",
+    expanded: true,
+    components: [
+      {
+        img: 'images/yeoman.png',
+        title: 'Transformer',
+        hotkey: 'T'
+      },
+      {
+        img: 'images/yeoman.png',
+        title: 'Resistor',
+        hotkey: 'R'
+      },
+      {
+        img: 'images/yeoman.png',
+        title: 'Wire',
+        hotkey: 'W'
+      }
+    ]
+  }
 ];
+
+
+const ElementButtonGroup = (
+    sectionName,
+    expanded,
+    buttonData
+) => (
+    <Card expandable={true} initiallyExpanded={true}>
+      <CardHeader title={sectionName} actAsExpander={true} showExpandableButton={true}/>
+      <CardText expandable={true} style={{padding: 5}}>
+        <div style={styles.root}>
+          {
+            buttonData.map((button, i) => (
+                <ComponentButton hotkey={button.hotkey} key={i}>
+                  {button.title}
+                </ComponentButton>
+            ))
+          }
+        </div>
+      </CardText>
+    </Card>
+);
 
 class ElementPane extends React.Component {
   render() {
     return (
-      <Drawer width={150} open={true} docked={true}>
-        <div style={styles.root}>
-          <Subheader>December</Subheader>
-          {tilesData.map((tile, i) => (
-
-            <ComponentButton hotkey="W" key={i}>
-              Transformer
-            </ComponentButton>
-          ))}
-        </div>
-      </Drawer>
+        <Drawer width={200} open={true} docked={true}>
+          {
+            tilesData.map((item, i) => (
+                ElementButtonGroup(item.sectionName, true, item.components)
+            ))
+          }
+        </Drawer>
     );
   }
 }
