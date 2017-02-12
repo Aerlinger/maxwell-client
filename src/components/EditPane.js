464cb67e47963826f17568e3b803a2c0ae07f266
Anthony Erlinger <aerlinger@gmail.com>
2017-02-12 00:20:14 -0500

    Add functions to EditPane

--- src/components/EditPane.js
+++ src/components/EditPane.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {List, ListItem} from 'material-ui/List';
 import Divider from 'material-ui/Divider';
-import Checkbox from 'material-ui/Checkbox';
 import Toggle from 'material-ui/Toggle';
 import TextField from 'material-ui/TextField';
 import SelectField from 'material-ui/SelectField';
@@ -11,125 +10,165 @@ import Avatar from 'material-ui/Avatar';
 import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table';
 import Slider from 'material-ui/Slider';
 
-import SidePaneStyles from '../styles/SidePane.css';
-
-
-const styles = {
-  block: {
-    maxWidth: 250
-  },
-  toggle: {
-    marginBottom: 16
-  },
-  thumbOff: {
-    backgroundColor: '#ffcccc'
-  },
-  trackOff: {
-    backgroundColor: '#ff9d9d'
-  },
-  thumbSwitched: {
-    backgroundColor: 'red'
-  },
-  trackSwitched: {
-    backgroundColor: '#ff9d9d'
-  },
-  labelStyle: {
-    color: 'red'
-  }
-};
 
 const fields = {
-  name: "Resistor",
-  icon: "Resistor.png",
-  description: "Description of component",
+  name: 'BJT',
+  icon: 'BJT.png',
+  description: 'Description of component',
+  label: 'user_label',
   voltage: 10,
   current: 5,
   power: 1,
   params: [
-
     {
-      name: "Polarity",
+      name: 'polarity',
+      title: 'Polarity',
       value: 1,
-      description: "Current multiplier",
       default_value: -1,
       data_type: Math.sign,
-      field_type: "select",
-      select_values: {"NPN": -1, "PNP": 1}
+      field_type: 'select',
+      select_values: {'NPN': -1, 'PNP': 1}
     },
     {
-      name: "VBE",
+      name: 'vbe',
+      title: 'Base-Emitter voltage',
       value: 1,
-      unit: "Voltage",
-      symbol: "V",
+      unit: 'Voltage',
+      symbol: 'V',
       default_value: 0,
       data_type: parseFloat
     },
     {
-      name: "Beta",
+      name: 'vbc',
+      title: 'Base-Collector voltage',
+      value: 0.05,
+      unit: 'Voltage',
+      symbol: 'V',
+      default_value: 0,
+      data_type: parseFloat
+    },
+    {
+      name: 'beta',
+      title: 'Current gain',
+      hint: 'Ratio of collector current to base current',
       value: 100,
-      description: "Current gain",
+      description: 'Current gain',
       default_value: 100,
       data_type: parseFloat,
       range: [0, Infinity]
     },
     {
-      name: "Show current",
+      name: 'show_current',
+      title: 'Show current',
       value: true,
-      description: "Show current through gate",
+      description: 'Show current through gate',
       default_value: true,
-      data_type: (val) => { !!val },
+      field_type: 'boolean',
+      data_type: (val) => {
+        !!val
+      },
       range: [0, Infinity]
+    },
+    {
+      name: 'duty_cycle',
+      title: 'Duty Cycle',
+      hint: 'Percentage of high voltage at output',
+      value: 50,
+      step: 1,
+      description: 'Show current through gate',
+      default_value: true,
+      field_type: 'slider',
+      toVal: (x) => x / 100,
+      range: [0, 100]
     }
   ]
 };
 
-// TODO: need to update state on <SelectField> to set value on MenuItem
-const ParamSelectField = ({
-    name,
-    select_values,
-    value
-}) => (
-    <SelectField
-        floatingLabelText={name}
-        value={value}
-    >
-      {
-        Object.keys(select_values).map((key, index) => (
-          <MenuItem value={select_values[key]} key={index} primaryText={key}/>
-        ))
-      }
-    </SelectField>
-);
+class EditPane extends React.Component {
+  constructor(props) {
+    super(props);
 
-const ParamTextField = ({
-    name,
-    value
-}) => (
-    <TextField
+    this.state = {
+      name: 'BJT',
+      icon: 'BJT.png',
+      description: 'Description of component',
+      label: 'user_label',
+      voltage: 10,
+      current: 5,
+      power: 1,
+      params: {
+        polarity: 1,
+        vbe: 1,
+        vbc: 0.05,
+        beta: 100,
+        show_current: true,
+        duty_cycle: 50
+      }
+    }
+  }
+
+  addSelectField({
+      name,
+      title,
+      hint,
+      value,
+      default_value,
+      data_type,
+      field_type,
+      select_values
+  }) {
+    let raw_value = value ? value : default_value;
+
+    return (
+        <SelectField
+            floatingLabelText={title}
+            value={raw_value}
+        >
+          {
+            Object.keys(select_values).map((key, index) => (
+                <MenuItem value={select_values[key]} key={index} primaryText={key}/>
+            ))
+          }
+        </SelectField>
+    )
+  }
+
+  addTextField({
+      name,
+      title,
+      hint,
+      value,
+      default_value,
+      data_type,
+      field_type
+  }) {
+    return (<TextField
         hintText={name}
         floatingLabelText={name}
         floatingLabelFixed={true}
         value={value}
-    />
-);
+    />);
+  }
 
-const ParamBooleanField = ({
-    name,
-    value
-}) => (
-  <Toggle label={name} labelPosition="right" value={value}/>
-);
+  addBooleanField({
+      name,
+      title,
+      hint,
+      value,
+      default_value,
+      data_type
+  }) {
+    return (<Toggle label={name} labelPosition='right' value={value}/>);
+  }
 
-
-class EditPane extends React.Component {
   render() {
     return (
         <Drawer width={300} openSecondary={true} open={true}>
           <List>
             <ListItem
-                primaryText="ResistorElm"
-                leftAvatar={<Avatar src="images/yeoman.png"/>}
-                secondaryText="Description"
+                primaryText='ResistorElm'
+                leftAvatar={<Avatar src='images/yeoman.png'/>}
+                secondaryText='Description'
             />
 
             <Divider />
@@ -147,9 +186,9 @@ class EditPane extends React.Component {
             </Table>
             <Divider />
 
-            { ParamBooleanField({name: "Sample", value: true}) }
-            { ParamTextField({name: "Sample", value: "Text value"}) }
-            { ParamSelectField({name: "Sample", select_values: {"NPN": -1, "PNP": 1}, value: 1}) }
+            { this.addBooleanField({name: 'Sample', value: true}) }
+            { this.addTextField({name: 'Sample', value: 'Text value'}) }
+            { this.addSelectField({title: 'Sample', select_values: {'NPN': -1, 'PNP': 1}, value: 1}) }
 
           </List>
         </Drawer>
