8e922ca7407808ba43b780c69156aed913982412
Anthony Erlinger <aerlinger@gmail.com>
2017-02-11 22:16:00 -0500

    Extract form elements in EditPane

--- src/components/EditPane.js
+++ src/components/EditPane.js
@@ -38,6 +38,89 @@ const styles = {
   }
 };
 
+const fields = {
+  name: "Resistor",
+  icon: "Resistor.png",
+  description: "Description of component",
+  voltage: 10,
+  current: 5,
+  power: 1,
+  params: [
+
+    {
+      name: "Polarity",
+      value: 1,
+      description: "Current multiplier",
+      default_value: -1,
+      data_type: Math.sign,
+      field_type: "select",
+      select_values: {"NPN": -1, "PNP": 1}
+    },
+    {
+      name: "VBE",
+      value: 1,
+      unit: "Voltage",
+      symbol: "V",
+      default_value: 0,
+      data_type: parseFloat
+    },
+    {
+      name: "Beta",
+      value: 100,
+      description: "Current gain",
+      default_value: 100,
+      data_type: parseFloat,
+      range: [0, Infinity]
+    },
+    {
+      name: "Show current",
+      value: true,
+      description: "Show current through gate",
+      default_value: true,
+      data_type: (val) => { !!val },
+      range: [0, Infinity]
+    }
+  ]
+};
+
+// TODO: need to update state on <SelectField> to set value on MenuItem
+const ParamSelectField = ({
+    name,
+    select_values,
+    value
+}) => (
+    <SelectField
+        floatingLabelText={name}
+        value={value}
+    >
+      {
+        Object.keys(select_values).map((key, index) => (
+          <MenuItem value={select_values[key]} key={index} primaryText={key}/>
+        ))
+      }
+    </SelectField>
+);
+
+const ParamTextField = ({
+    name,
+    value
+}) => (
+    <TextField
+        hintText={name}
+        floatingLabelText={name}
+        floatingLabelFixed={true}
+        value={value}
+    />
+);
+
+const ParamBooleanField = ({
+    name,
+    value
+}) => (
+  <Toggle label={name} labelPosition="right" value={value}/>
+);
+
+
 class EditPane extends React.Component {
   render() {
     return (
@@ -51,12 +134,6 @@ class EditPane extends React.Component {
 
             <Divider />
             <Table selectable={false}>
-              <TableHeader displayRowCheckbox={false}>
-                <TableRow>
-                  <TableHeaderColumn>Name</TableHeaderColumn>
-                  <TableHeaderColumn>Status</TableHeaderColumn>
-                </TableRow>
-              </TableHeader>
               <TableBody displayRowCheckbox={false}>
                 <TableRow>
                   <TableRowColumn>Voltage</TableRowColumn>
@@ -70,71 +147,10 @@ class EditPane extends React.Component {
             </Table>
             <Divider />
 
-            <ListItem>
+            { ParamBooleanField({name: "Sample", value: true}) }
+            { ParamTextField({name: "Sample", value: "Text value"}) }
+            { ParamSelectField({name: "Sample", select_values: {"NPN": -1, "PNP": 1}, value: 1}) }
 
-              <SelectField
-                  floatingLabelText="Frequency"
-                  value={1}
-              >
-                <MenuItem value={1} primaryText="Never"/>
-                <MenuItem value={2} primaryText="Every Night"/>
-                <MenuItem value={3} primaryText="Weeknights"/>
-                <MenuItem value={4} primaryText="Weekends"/>
-                <MenuItem value={5} primaryText="Weekly"/>
-              </SelectField>
-            </ListItem>
-
-            <ListItem>
-              <TextField
-                  hintText="Hint Text"
-                  floatingLabelText="Fixed Floating Label Text"
-                  floatingLabelFixed={true}
-              />
-            </ListItem>
-
-            <ListItem primaryText="Events and reminders" rightToggle={<Toggle />}/>
-
-            <ListItem>
-              <Slider defaultValue={0.5} axis="x-reverse" />
-            </ListItem>
-
-            <Divider />
-
-            <TextField
-                hintText="Hint Text"
-                floatingLabelText="Fixed Floating Label Text"
-                floatingLabelFixed={true}
-            />
-
-            <TextField
-                hintText="Hint Text"
-                floatingLabelText="Fixed Floating Label Text"
-                floatingLabelFixed={true}
-            />
-
-            <SelectField
-                floatingLabelText="Frequency"
-                value={1}
-            >
-              <MenuItem value={1} primaryText="Never"/>
-              <MenuItem value={2} primaryText="Every Night"/>
-              <MenuItem value={3} primaryText="Weeknights"/>
-              <MenuItem value={4} primaryText="Weekends"/>
-              <MenuItem value={5} primaryText="Weekly"/>
-            </SelectField>
-
-            <Toggle
-                label="Styling"
-                thumbStyle={styles.thumbOff}
-                trackStyle={styles.trackOff}
-                thumbSwitchedStyle={styles.thumbSwitched}
-                trackSwitchedStyle={styles.trackSwitched}
-                labelStyle={styles.labelStyle}
-            />
-
-            <Toggle label="Label on the right" labelPosition="right" style={styles.toggle}/>
-
-            <Slider defaultValue={0.5} axis="x-reverse" />
           </List>
         </Drawer>
     );
