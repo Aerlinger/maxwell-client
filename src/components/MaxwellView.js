c2c6d964d0d0d879eceffe689ca360e9b066af5f
Anthony Erlinger <aerlinger@gmail.com>
2017-02-16 03:02:44 -0500

    Add performance meter streaming plot

--- src/components/MaxwellView.js
+++ src/components/MaxwellView.js
@@ -7,15 +7,24 @@ class MaxwellView extends React.Component {
 
   componentDidMount() {
     /*
-    Maxwell.createContext("ohms", "../circuits/v4/#{circuit_name}.json", canvas, function (circuitContext) {
+     Maxwell.createContext("ohms", "../circuits/v4/#{circuit_name}.json", canvas, function (circuitContext) {
 
-    });
-    */
+     });
+     */
   }
 
   render() {
     return (
-      <FlexCanvas topMargin={50} leftMargin={100} onResize={() => {console.log("RESIZE")}} ref={(component) => {this.canvas = component}}/>
+        <div>
+          <canvas id="performance_sparkline" width="200" height="40"></canvas>
+
+          <FlexCanvas topMargin={50} leftMargin={100} onResize={() => {
+            console.log("RESIZE")
+          }} ref={(component) => {
+            this.canvas = component
+          }}/>
+
+        </div>
     )
   }
 }
