efe2ff75f482161e9c8e02d7fc02a57d1c85159d
Anthony Erlinger <aerlinger@gmail.com>
2017-02-16 17:21:21 -0500

    Add initial Express App and serve from public/

--- server.js
+++ server.js
@@ -1,38 +1,16 @@
-/*eslint no-console:0 */
-'use strict';
-require('core-js/fn/object/assign');
-const webpack = require('webpack');
-const WebpackDevServer = require('webpack-dev-server');
-const config = require('./webpack.config');
-const open = require('open');
+var express = require('express');
+var path = require('path');
 
-/**
- * Flag indicating whether webpack compiled for the first time.
- * @type {boolean}
- */
-let isInitialCompilation = true;
+var app = express();
 
-const compiler = webpack(config);
+var isProduction = process.env.NODE_ENV === 'production';
+var port = isProduction ? process.env.PORT : 3000;
+var publicPath = path.resolve(__dirname, 'public');
 
-let server = new WebpackDevServer(compiler, config.devServer);
+// We point to our static assets
+app.use(express.static(publicPath));
 
-server.listen(config.port, 'localhost', (err) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log('Listening at localhost:' + config.port);
-});
-
-compiler.plugin('done', () => {
-  if (isInitialCompilation) {
-    // Ensures that we log after webpack printed its stats (is there a better way?)
-    setTimeout(() => {
-      console.log('\n✓ The bundle is now ready for serving!\n');
-      console.log('  Open in iframe mode:\t\x1b[33m%s\x1b[0m',  'http://localhost:' + config.port + '/webpack-dev-server/');
-      console.log('  Open in inline mode:\t\x1b[33m%s\x1b[0m', 'http://localhost:' + config.port + '/\n');
-      console.log('  \x1b[33mHMR is active\x1b[0m. The bundle will automatically rebuild and live-update on changes.')
-    }, 350);
-  }
-
-  isInitialCompilation = false;
+// And run the server
+app.listen(port, function () {
+  console.log('Server running on port ' + port);
 });
