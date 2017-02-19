5984a78b84cd06202194431c0f37d434d9c87928
Anthony Erlinger <aerlinger@gmail.com>
2017-02-19 00:53:19 -0500

    Very basic JSON representation for menu

---
+++ src/components/MenuBar.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import Auth from '../modules/Auth';
+import FontIcon from 'material-ui/FontIcon';
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
+import RaisedButton from 'material-ui/RaisedButton';
+
+import MainToolbarStyle from '../styles/MainToolbar.css';
+import ToolbarMenuItem from './ToolbarMenuItem';
+
+import jQuery from 'jquery'
+
+const style = {
+  button: {
+    margin: 12
+  }
+};
+
+let menu = {
+  main: {
+    "New Circuit": {},
+    "Save Circuit": {},
+    "Share...": {},
+    "Load Existing...": {},
+    "Load From JSON": {},
+    "Export to schematic...": {},
+    "Export JSON": {},
+    "Report a bug...": {}
+  },
+  edit: {
+    "Undo": {},
+    "Redo": {},
+    "Cut": {},
+    "Copy": {},
+    "Paste": {},
+    "Duplicate": {},
+    "Select All": {},
+  },
+  run: {
+    "run": {},
+    "pause": {},
+    "restart": {},
+    "step_forward": {},
+  },
+  create: {
+    "wire": {},
+    "resistor": {},
+    "ground": {},
+    "voltagesource": {},
+    "Paste": {},
+    "Duplicate": {},
+    "Select All": {},
+    "Add probe at selected node": {},
+    "Add probe at selected element": {},
+  },
+  options: {
+    "Options...": {},
+    "Advanced Options...": {},
+  },
+  circuits: {
+  }
+};
+
+class MenuBar extends React.Component {
+}
+
+export default MenuBar;
+
